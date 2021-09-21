eae25f5f0da35354a113759c01fc61df764891b2
KyeongHwanMin <h45652@naver.com>
2021-09-21 19:57:54 +0900

    refactor(index.js) : JSX에 표현식 포함하기

--- blog/blog/src/index.js
+++ blog/blog/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
