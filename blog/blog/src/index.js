b307d60bf609ae4d01be9050bf4f9129a64636b8
KyeongHwanMin <h45652@naver.com>
2021-09-23 16:01:13 +0900

    refactor(index.js) : DOM에 엘리먼트 렌더링하기

--- blog/blog/src/index.js
+++ blog/blog/src/index.js
@@ -5,25 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
