aac5278fed8cb773995558773ef5d0502736d150
KyeongHwanMin <h45652@naver.com>
2021-09-23 16:06:18 +0900

    refactor(index.js) : 시간표시

--- blog/blog/src/index.js
+++ blog/blog/src/index.js
@@ -5,10 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(element, document.getElementById('root'));
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
 
-
+setInterval(tick, 1000);
 
 
 
