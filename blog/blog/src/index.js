2f4a6bad3d90e089362050e51f8441b751cc536d
KyeongHwanMin <h45652@naver.com>
2021-09-24 16:53:45 +0900

    refactor(index.js) : textarea 태그 ( value를 사용)

--- blog/blog/src/index.js
+++ blog/blog/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
-class NameForm extends React.Component {
+class EssayForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,7 +20,7 @@ class NameForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('An essay was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
@@ -26,8 +28,8 @@ class NameForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -36,7 +38,7 @@ class NameForm extends React.Component {
 }
 
 ReactDOM.render(
-  <NameForm />,
+  <EssayForm />,
   document.getElementById('root')
 );
 
